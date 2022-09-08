61d17d241b251638287f6f1b5e25fdc4750a2f69
Barneslow <darrachb1991@gmail.com>
2022-09-08 19:55:06 +0100

    baseURl

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,3 +1,3 @@
-// export const baseUrl = "https://barneslow-productivity.herokuapp.com";
+export const baseUrl = "https://barneslow-productivity.herokuapp.com";
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
