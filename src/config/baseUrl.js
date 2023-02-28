f26ee0c376594f010e74016190d0aa51040116fd
Darrach Barneveld <darrachbarneveld@Darrachs-MacBook-Pro.local>
2023-02-28 04:44:01 +0000

    updated baseurl for testing

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = "https://productivity-backend-production.up.railway.app";
+// export const baseUrl = "https://productivity-backend-production.up.railway.app";
 
-// export const baseUrl = "http://localhost:6000";
+export const baseUrl = "http://localhost:80";
